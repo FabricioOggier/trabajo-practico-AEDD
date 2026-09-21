e60fa714425c50610f9c8e37c206db4470511b36
FabricioOggier <fabriciooggier@gmail.com>
2026-09-21 17:57:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,5 +8,60 @@ using namespace std;
 
 int main() {
 
+    Presentacion();
+	char eleccion;
+	bool condicion=false;
+	do{
+        Selector();
+		cin>>eleccion;
+		switch (eleccion){
+		case '1':
+			if(!condicion){
+				inicioSesion();
+				condicion=true;
+			}
+			else{
+				cout<<"Ya se ha iniciado sesión"<<endl;
+			}
+			break;
+		case '2':
+			if(condicion){
+				EstadoJugadores();
+			}
+			else{
+				cout<<"Debes iniciar sesión para acceder a esta función"<<endl;
+			}
+			break;
+		case '3':
+		   if(condicion){
+			   HistorialGiros();
+		   }
+		   else{
+			   cout<<"Debes iniciar sesión para acceder a esta función"<<endl;
+		   }
+		   break;
+		case '4':
+			if(condicion){
+				Estadisticas();
+			}
+			else{
+				cout<<"Debes iniciar sesión para acceder a esta función"<<endl;
+			}
+			break;
+		case '5':
+		case '6':
+		case '7':
+			cout<<"Funcionalidad en Desarrollo"<<endl;
+			break;
+		case 'X':
+		case 'x':
+			break;
+		default:
+			cout<<"Función desconocida"<<endl;
+		};
+			
+			
+	}while(eleccion!='x' and eleccion!='X') ;
+    
     return 0;
 }
