649af12e38812c3360c0312e8db005854239268b
Lautaro Peresin <lautaroperesin@gmail.com>
2026-09-21 17:18:08 -0300

    Primer commit

---
+++ jugador.h
@@ -0,0 +1,27 @@
+
+#ifndef JUGADOR_H
+#define JUGADOR_H
+#include <string>
+using namespace std;
+
+struct Apuesta{ 
+	int fichasJugadas; 
+	int numeroApostado; 
+	char colorApostado; 
+	int tipoApuesta; // Identificador del tipo de apuesta seleccionada 1-Numero, 2-Color, 3-Par, 4-Impar
+	
+};
+struct Jugador { 
+	string nombre; 
+	int fichas; 
+	Apuesta apuestas[MAX_APUESTAS]; 
+	int tlApuestas; 
+	int victorias;
+	int derrotas;
+};
+
+
+
+void cargarJugadores(Jugador jugadores[], int &cant);
+
+#endif
