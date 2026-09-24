6e29878d7f64dfac3d3fba2d6632a7cfe29b18b2
Federico Cian <fedecian31@gmail.com>
2026-09-24 18:46:13 -0300

    Agrego condicion de finalizacion

---
+++ condicion.cpp
@@ -0,0 +1,34 @@
+#include "condicion.h"
+
+/************************************************************
+* Función: condicionfinal
+* Parámetros:
+* - Jugador jugadores[] : Arreglo de jugadores.
+* - int cant : Cantidad de jugadores.
+* Retorna:
+* - bool : Retorna true si se cumple alguna condición de
+*          finalización y false en caso contrario.
+* Descripción:
+* Verifica si la partida debe finalizar. Recorre todos los
+* jugadores comprobando si alguno se quedó sin fichas.
+* También verifica si se alcanzó el límite de 1200 apuestas
+* y el estado de la variable seguirjugando.
+* Si alguna de estas condiciones se cumple, retorna true;
+* de lo contrario, retorna false.
+*************************************************************/
+
+bool condicionfinal(Jugador jugadores[], int cant, bool seguirjugando, int totalGiros){
+	bool fin = false;
+	for(int i=0;i<cant;i++) { 
+		if(jugadores[i].fichas == 0){
+			fin = true;
+		}
+	}
+	if(totalGiros == 1200){
+		fin = true;
+	}
+	if(!seguirjugando){
+		fin = true;
+	}
+	return fin;
+}
---
+++ condicion.h
@@ -0,0 +1,8 @@
+#ifndef CONDICION_H
+#define CONDICION_H
+#include "jugador.h"
+
+
+bool condicionfinal(Jugador jugadores[], int cant, bool seguirjugando, int totalGiros);
+
+#endif
